1d2bebcb8a2864410bd131fb9c41c0f68bb9242b
Tatsu Nana <tatsunana@Tatsus-MacBook-Air.local>
2024-10-31 01:06:48 +1100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
+import Header from "./_components/header";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -12,6 +13,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
+      <Header />
       <main className="flex min-h-screen flex-col items-center justify-center  text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
